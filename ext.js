172d4735d51417dc888d2b1ae642ce708444dca9
TylerDevers <devers.tyler@gmail.com>
2017-08-16 14:40:21 -0700

    added a start button that begins the game and changes to a reset button. Works as desired. Now code player turns

--- ext.js
+++ ext.js
@@ -1,14 +1,33 @@
-var buttonsIndex, gameOn, user, computer;
+var buttonsIndex, gameOn, user, computer, turn;
 
 buttonsIndex = document.getElementsByTagName('a');
 gameOn = false;
 user = 'x';
 computer = 'o';
+turn = "x";
+
 
 //changes the game spaces to x and y values.
 function clicked(text) {	
-	text.innerHTML = "x";
-	console.log(text);
+	if (gameOn){
+		text.innerHTML = "x";
+	}
 }
 
+function start(text) {
+		var classSwitch = document.getElementById("start");
+		if (gameOn) {
+			text.innerHTML = "Start";
+			for (var i = 1; i<buttonsIndex.length; i++) { 
+				//starts at index 1 to prevent start button from changing.
+				buttonsIndex[i].innerHTML = "^";
+			}
+			classSwitch.className = "btn btn-default";
+			gameOn = false;
+		} else if (!gameOn) {
+			text.innerHTML = "Reset";
+			classSwitch.className = "btn btn-warning";
+			gameOn = true;	
+		}
+}
 console.log('loaded');
